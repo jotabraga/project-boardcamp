c7bc98a68490598c445d0ef0494d035f83c039a9
jotabraga <jpmbraga.feg@gmail.com>
2021-06-16 21:21:10 -0300

    Start of configuration issues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import customExpress from './config/customExpress.js';
+import routes from './routes/routes.js';
+
+
 
 const app = customExpress();
+routes(app);
+
+
+app.listen(4000, () => {
+    console.log("Jota server online on port 4000");
+  });
