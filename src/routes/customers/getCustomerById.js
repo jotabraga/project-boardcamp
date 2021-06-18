3afdb3a51c62494c1dee66f7560e0ae90587ec57
jotabraga <jpmbraga.feg@gmail.com>
2021-06-17 22:02:50 -0300

    Get customer by id route done

--- src/routes/customers/getCustomerById.js
+++ src/routes/customers/getCustomerById.js
@@ -1,12 +1,12 @@
 export default function readCategories(app, connection) {
 
-    app.get("/customers", async (req, res) => {
+    app.get("/customers/:id", async (req, res) => {
 
-      const cpf = req.query.cpf ?? "";
+      const id = req.params;
     
       try {
-        const sql = 'select * from customers where customers.cpf ilike $1';
-        const customersList = await connection.query(sql, [cpf + "%"]);
+        const sql = 'select * from customers where id like $1';
+        const customersList = await connection.query(sql, [id]);
         res.send(customersList.rows);  
       } catch (error) {
         console.log(error);
